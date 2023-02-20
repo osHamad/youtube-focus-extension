2fba7324b8a897de68160d91d3be6402c017f7ee
Osama Hamad <osamahama2005@gmail.com>
2023-02-20 15:52:30 -0500

    fix- hide home page after navigating from video link

--- scripts/home.js
+++ scripts/home.js
@@ -1,16 +1,28 @@
-let a = document.getElementsByTagName("ytd-page-manager")[0]
-a.style.display = 'none'
+document.getElementById("logo").addEventListener("click", ()=>{
+    disableThing()
+})
 
-interval = setInterval(
-    () => {
-        let browse = document.getElementsByTagName("ytd-browse")[0]
-        if (browse) {
-            clearInterval(interval)
-            browse.style.display = "none"
-            a.style.display = "flex"
+disableThing = () => {
+    let a = document.getElementsByTagName("ytd-page-manager")[0]
+    a.style.display = 'none'
+
+    interval = setInterval(
+        () => {
+            let browse = document.getElementsByTagName("ytd-rich-grid-renderer")[0]
+            if (browse) {
+                clearInterval(interval)
+                browse.style.display = "none"
+                a.style.display = "flex"
+            }
         }
-    }
-, 100)
+    , 100)
+}
+
+if (window.location.pathname == "/") {
+    disableThing()
+}
+
+
 
 
 
