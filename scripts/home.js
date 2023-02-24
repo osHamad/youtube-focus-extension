8bd2abffe37658b6c1cb6c350d4c0f94d5c7d02a
Osama Hamad <osamahama2005@gmail.com>
2023-02-24 11:46:01 -0500

    add load screen before hiding home

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,65 @@
-document.getElementById("logo").addEventListener("click", ()=>{
-    disableThing()
-})
+createLoadScreen = () => {
+    // Create loading screen body
+    var loadScreen = document.createElement("body");
+    loadScreen.id = "loading-screen"
+    
+    // Create loading screen div
+    var loadDiv = document.createElement("div");
+    loadDiv.style.width = "100%"
+    loadDiv.style.height = "100%"
+    loadDiv.style.backgroundColor = "rgb(15, 15, 15)"
+    
+    loadDiv.style.position = "fixed"
+    loadDiv.style.zIndex = "10000"
+    
+    // Add load div to the body container
+    loadScreen.appendChild(loadDiv);
 
-disableThing = () => {
-    let a = document.getElementsByTagName("ytd-page-manager")[0]
-    a.style.display = 'none'
+    // Set up html page for loader
+    var htmlPage = document.getElementsByTagName("html")[0]
+    htmlPage.style.height = "100%"  // make height 100 to inherit loader
+    htmlPage.appendChild(loadScreen);   // add load body to html page
+}
 
-    interval = setInterval(
+minimizeSideBar = () => {
+    let sidebar = document.getElementsByTagName("tp-yt-app-drawer")[0]
+    sidebar.style.display = 'none'
+    document.getElementById("guide-button").style.display = 'none'
+
+    let mini = document.getElementsByTagName("ytd-mini-guide-renderer")[0]
+    mini.removeAttribute('hidden')
+    mini.removeAttribute('disable-upgrade')
+    mini.removeAttribute('guide-persistent-and-visible')
+    mini.setAttribute('mini-guide-visible', 'mini-guide-visible')
+}
+
+hideHomePage = () => {
+
+    createLoadScreen()
+
+    let interval = setInterval(
         () => {
             let browse = document.getElementsByTagName("ytd-rich-grid-renderer")[0]
             if (browse) {
                 clearInterval(interval)
                 browse.style.display = "none"
-                a.style.display = "flex"
+                document.getElementById("loading-screen").remove()
+                document.getElementById("logo").addEventListener("click", ()=>{
+                    hideHomePage()
+                })
+                minimizeSideBar()
+
             }
         }
-    , 100)
+    , 500)
+
 }
+
+
 
 if (window.location.pathname == "/") {
-    disableThing()
+    hideHomePage()
 }
-
-
-
 
 
 
